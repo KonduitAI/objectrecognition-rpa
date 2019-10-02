39dfbdb0b3cbb46c7c4de7372bee8efd6e0f1280
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2019-10-02 16:30:51 +0900

    'update'

---
+++ scripts/predict_exec.py
@@ -0,0 +1,28 @@
+# Note the sys declaration here is to sync with the server's python run time.
+import sys
+sys.version = '3.7.3 | packaged by conda-forge | (default, Jul  1 2019, 21:52:21)\n[GCC 7.3.0]'
+
+# Run the pythons cript
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+import tensorflow as tf
+import keras
+
+# needed when dealing with multi threading
+sess = tf.Session()
+keras.backend.set_session(sess)
+
+model = ResNet50(weights='imagenet')
+
+#img_path = 'African_Bush_Elephant_1-783x1024.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+decoded = decode_predictions(preds, top=3)[0]
+id,label,proba = decoded[0]
+proba = str(proba)
